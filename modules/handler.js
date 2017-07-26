67597efdca6d598f8eee8498784ee9df1d8833df
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-07-26 12:05:29 +0100

    created a 'step1' solution

--- modules/handler.js
+++ modules/handler.js
@@ -4,24 +4,24 @@ const createRecord = require('./createRecord.js');
 const validateFilename = require('./validateFilename.js');
 
 module.exports = (req, res, body) => {
-  // const payload = JSON.parse(body);
-  // var filename = payload.filename;
-  // var contents = payload.contents;
-  //
-  // const result = validateFilename(filename);
-  // if (!result.isValid) {
-  //   res.writeHead(400, {'Content-Type': 'application/json'});
-  //   res.end(JSON.stringify(`Invalid parameters: ${result.message}`));
-  //   return;
-  // }
-  //
-  // createRecord(filename, contents, (err, message) => {
-  //   if (err) {
-  //     res.writeHead(500, {'Content-Type': 'application/json'});
-  //     res.end(JSON.stringify({error: 'internal error'}));
-  //   } else {
-  //     res.writeHead(200, {'Content-Type': 'application/json'});
-  //     res.end(JSON.stringify(message));
-  //   }
-  // });
+  const payload = JSON.parse(body);
+  var filename = payload.filename;
+  var contents = payload.contents;
+
+  const result = validateFilename(filename);
+  if (!result.isValid) {
+    res.writeHead(400, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(`Invalid parameters: ${result.message}`));
+    return;
+  }
+
+  createRecord(filename, contents, (err, message) => {
+    if (err) {
+      res.writeHead(500, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({error: 'internal error'}));
+    } else {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(message));
+    }
+  });
 };
--- tests/modules/index.test.js
+++ tests/modules/index.test.js
@@ -4,42 +4,43 @@ const tape = require('tape');
 const shot = require('shot');
 const router = require('../../modules');
 //
-// tape('Filename is not a string', t => {
-//   shot.inject(
-//     router,
-//     {
-//       method: 'POST',
-//       url: '/submit',
-//       payload: {
-//         filename: 12,
-//         contents: {name: 'example', age: 4, body: 'example'}
-//       }
-//     },
-//     res => {
-//       t.equal(res.statusCode, 400, res.payload);
-//       t.end();
-//     }
-//   );
-// });
-//
-// tape('Example correct request', t => {
-//   shot.inject(
-//     router,
-//     {
-//       method: 'POST',
-//       url: '/submit',
-//       payload: {
-//         filename: 'filename',
-//         contents: {name: 'example', age: 4, body: 'example'}
-//       }
-//     },
-//     res => {
-//       t.equal(res.statusCode, 200, res.payload);
-//       t.end();
-//     }
-//   );
-// });
+tape('Filename is not a string', t => {
+  shot.inject(
+    router,
+    {
+      method: 'POST',
+      url: '/submit',
+      payload: {
+        filename: 12,
+        contents: {name: 'example', age: 4, body: 'example'}
+      }
+    },
+    res => {
+      t.equal(res.statusCode, 400, res.payload);
+      t.end();
+    }
+  );
+});
 
+tape('Example correct request', t => {
+  shot.inject(
+    router,
+    {
+      method: 'POST',
+      url: '/submit',
+      payload: {
+        filename: 'filename',
+        contents: {name: 'example', age: 4, body: 'example'}
+      }
+    },
+    res => {
+      t.equal(res.statusCode, 200, res.payload);
+      t.end();
+    }
+  );
+});
+
+//Test if the name is too long - currently failing as I haven't writen a fucntion for it yet
 // tape('Too long filename', t => {
 //   shot.inject(
 //     router,
